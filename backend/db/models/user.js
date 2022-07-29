664ca7b015cc854a83fabef9dfaeea2bce13bc62
JColonJr <97004744+JColonJr@users.noreply.github.com>
2022-07-29 00:40:39 -0400

    Add User logout backend endpoint

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -18,6 +18,9 @@ module.exports = (sequelize, DataTypes) => {
       const { id, username, email } = this; // context will be the User instance
       return { id, username, email };
     }
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
     static getCurrentUserById(id) {
       return User.scope("currentUser").findByPk(id);
     }
